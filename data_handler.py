78492a9e091ffb8c51afdc3e4f285de4127c1b4f
Krisztian Pometko <krisztian@pometko.hu>
2019-09-10 10:29:37 +0200

    Added: dictionary generator functions for adding questions and answers

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import time
 
 DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data.csv'
 DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_value', 'estimation', 'status']
@@ -36,3 +37,31 @@ def generate_id(stories):
     ordered_stories = sorted(stories, key=lambda x: DATA_HEADER[0])
     return str(int(ordered_stories[-1][DATA_HEADER[0]]) + 1)
 
+
+def gen_id():
+    return 0
+
+
+def generate_question_dict(data):
+    question_data = {}
+
+    question_data.update(id=gen_id())
+    question_data.update(submission_time=str(time.time()))
+    question_data.update(view_number=0)
+    question_data.update(vote_number=0)
+    question_data.update(title=data["title"])
+    question_data.update(message=data["message"])
+    question_data.update(image=data["image"])
+    return question_data
+
+
+def generate_answer_dict(data):
+    answer_data = {}
+
+    answer_data.update(id=gen_id())
+    answer_data.update(submission_time=str(time.time()))
+    answer_data.update(vote_number=0)
+    answer_data.update(question_id=data["question_id"])
+    answer_data.update(message=data["message"])
+    answer_data.update(image=data["image"])
+    return answer_data
