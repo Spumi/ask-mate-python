5fc985a8a9a95da110a2958c1b408cee3fffede8
Gergo Kovacs <kovacsgergo128@gmail.com>
2019-09-10 13:28:23 +0200

    Add /question/<question_id> route to server module

--- server.py
+++ server.py
@@ -24,5 +24,14 @@ def add_question():
     return render_template('add-question.html')
 
 
+@app.route('/question/<question_id>')
+def question_display(question_id):
+    question_database = {}
+    answer_database = {}
+    question = data_handler.get_question(question_id, question_database)
+    related_answers = data_handler.get_answers(question_id, answer_database)
+    return render_template('display_question.html', question=question, answers=related_answers)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
