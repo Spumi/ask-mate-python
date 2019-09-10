e660fcfb753b8ef351bdd34ad3760121f22c35d3
Rajnai Máté <ramate.rajnai@gmail.com>
2019-09-10 12:54:04 +0200

    Create list functionality

---
+++ data_handler.py
@@ -0,0 +1,87 @@
+import csv
+import os
+import time
+
+DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data.csv'
+DATA_HEADER = ['id', 'title', 'user_story', 'acceptance_criteria', 'business_value', 'estimation', 'status']
+STATUSES = ['planning', 'todo', 'in progress', 'review', 'done']
+
+
+def get_all_user_story():
+    with open(DATA_FILE_PATH, 'r', newline='') as f:
+        reader = csv.DictReader(f)
+        database = [dict(row) for row in reader]
+    return database
+
+
+def write_all_user_story(stories):
+    with open(DATA_FILE_PATH, 'w', newline='') as f:
+        writer = csv.DictWriter(f, DATA_HEADER)
+        writer.writeheader()
+        writer.writerows(stories)
+
+
+def add_story(stories, inputs):
+    stories.append(inputs)
+    return stories
+
+
+def update_story(stories, inputs):
+    for i, story in enumerate(stories):
+        if story[DATA_HEADER[0]] == inputs['id']:
+            stories[i] = inputs
+    return stories
+
+
+def generate_id(stories):
+    ordered_stories = sorted(stories, key=lambda x: DATA_HEADER[0])
+    return str(int(ordered_stories[-1][DATA_HEADER[0]]) + 1)
+
+
+def gen_id():
+    return 0
+
+
+def generate_question_dict(data):
+    question_data = {}
+
+    question_data.update(id=gen_id())
+    question_data.update(submission_time=str(time.time()))
+    question_data.update(view_number=0)
+    question_data.update(vote_number=0)
+    question_data.update(title=data["title"])
+    question_data.update(message=data["message"])
+    question_data.update(image=data["image"])
+    return question_data
+
+
+def generate_answer_dict(data):
+    answer_data = {}
+
+    answer_data.update(id=gen_id())
+    answer_data.update(submission_time=str(time.time()))
+    answer_data.update(vote_number=0)
+    answer_data.update(question_id=data["question_id"])
+    answer_data.update(message=data["message"])
+    answer_data.update(image=data["image"])
+    return answer_data
+
+
+def sorting_data(data, attribute, order_flag):
+    '''
+    :param attribute: list of dictionaries
+    :param attribute: By which the data is sorted-
+    :param order_flag: The order is ascending (False) or descending (True).
+    :return: The sorted data.
+    '''
+    try:
+        sorted_data = sorted(data, key=lambda x: int(x[attribute]) if x[attribute].isdigit() else x[attribute], reverse=order_flag)
+    except AttributeError:
+        sorted_data = sorted(data, key=lambda x: x[attribute], reverse=order_flag)
+    return sorted_data
+
+
+def temporary_get_data():
+    data = [{'id': 4, 'submission_time': 13, 'vote_number': '42', 'message': 'ersad'}, {'id': 2, 'submission_time': 123, 'vote_number': '25', 'message': 'csad'},
+     {'id': 3, 'submission_time': 123, 'vote_number': '32', 'message': 'asad'}, {'id': 1, 'submission_time': 73, 'vote_number': '32', 'message': 'abad'}]
+    return data
