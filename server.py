3c887bd06e6d932ddd380b50c8c1eca460ca4170
Gergo Kovacs <kovacsgergo128@gmail.com>
2019-09-11 11:40:14 +0200

    Add edit_question route to server

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 
 import data_handler
 
-from datetime import datetime
+from datetime import datetime, time
 
 app = Flask(__name__)
 
@@ -52,6 +52,21 @@ def question_display(question_id):
     question = data_handler.get_question(question_id, question_database)
     related_answers = data_handler.get_question_related_answers(question_id, answer_database)
     return render_template('display_question.html', question=question, answers=related_answers)
+
+
+@app.route('/<question_id>/edit', methods=['GET', 'POST'])
+def edit_question(question_id):
+
+    if request.method == 'POST':
+        edited_question_data = request.form.to_dict()
+        edited_question_data['submission_time'] = time()
+        question = data_handler.update_questions(question_id, edited_question_data)
+        return render_template('display_question.html', question=question)
+
+    all_questions = data_handler.get_questions()
+    question = data_handler.get_question(question_id, all_questions)
+
+    return render_template('edit-question.html', question=question)
 
 
 if __name__ == '__main__':
