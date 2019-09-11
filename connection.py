64d494b46ba6b1a8f00e743ef44fbcde946d22c5
Krisztian Pometko <krisztian@pometko.hu>
2019-09-11 11:36:35 +0200

    fixed list of  dict saving in connection module

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ import csv, os
 
 
 QUESTION_ANSWER_DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-ANSWER_DATA_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message,image']
+ANSWER_DATA_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 
 
 def csv_to_dict(file_path):
@@ -13,7 +13,7 @@ def csv_to_dict(file_path):
     return database
 
 
-def dict_to_csv(file_path, data, is_answers=True):
+def dict_to_csv(file_path, data, is_answers=False):
     with open(file_path, 'w', newline='') as f:
         writer = csv.DictWriter(f, ANSWER_DATA_HEADER if is_answers else QUESTION_ANSWER_DATA_HEADER)
         writer.writeheader()
