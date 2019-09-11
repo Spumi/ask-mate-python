9307755beeedbb373694f9d205ba837c2c07d8cc
Krisztian Pometko <krisztian@pometko.hu>
2019-09-11 18:42:30 +0200

    Refactor: Moved functions to util module

--- data_handler.py
+++ data_handler.py
@@ -1,11 +1,6 @@
-import csv
 import os
-import time
-from datetime import datetime
 
 # ANSWER_DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'data/answer.csv'
-import uuid
-from operator import itemgetter
 
 import connection
 
@@ -43,45 +38,6 @@ def modify_question_database(entry, is_question=True):
     connection.dict_to_csv(QUESTION_DATA_FILE_PATH if is_question else ANSWER_DATA_FILE_PATH, entry, False)
 
 
-def gen_question_id():
-    answers = get_questions()
-    items = [x['id'] for x in answers]
-    return int(max(items)) + 1
-
-
-def gen_answer_id():
-    answers = get_answers()
-    if len(answers) == 0:
-        return 0
-    items = [x['id'] for x in answers]
-    return int(max(items)) + 1
-
-
-def generate_question_dict(data):
-    question_data = {}
-
-    question_data.update(id=str(gen_question_id()))
-    question_data.update(submission_time=str(int(time.time())))
-    question_data.update(view_number=str(0))
-    question_data.update(vote_number=str(0))
-    question_data.update(title=data["title"])
-    question_data.update(message=data["message"])
-    question_data.update(image=data["image"])
-    return question_data
-
-
-def generate_answer_dict(data):
-    answer_data = {}
-
-    answer_data.update(id=str(gen_answer_id()))
-    answer_data.update(submission_time=str(int(time.time())))
-    answer_data.update(vote_number=str(0))
-    answer_data.update(question_id=data["question_id"])
-    answer_data.update(message=data["message"])
-    answer_data.update(image=data["image"])
-    return answer_data
-
-
 def get_question(question_id, question_database):
     for question_data in question_database:
         if question_data['id'] == question_id:
@@ -102,20 +58,6 @@ def get_question_related_answers(question_id, answer_database):
     return answers_of_question
 
 
-def sorting_data(data, attribute, order_flag):
-    '''
-    :param data: list of dictionaries
-    :param attribute: By which the data is sorted-
-    :param order_flag: The order is ascending (False) or descending (True).
-    :return: The sorted data.
-    '''
-    try:
-        sorted_data = sorted(data, key=lambda x: int(x[attribute]) if x[attribute].isdigit() else x[attribute], reverse=order_flag)
-    except AttributeError:
-        sorted_data = sorted(data, key=lambda x: x[attribute], reverse=order_flag)
-    return sorted_data
-
-
 def update_questions(question_id, updated_data):
     all_questions = get_questions()
     question = get_question(question_id, all_questions)
@@ -126,12 +68,6 @@ def update_questions(question_id, updated_data):
     all_questions[question_index] = question
     connection.dict_to_csv(QUESTION_DATA_FILE_PATH, all_questions)
     return question
-
-
-def convert_to_readable_date(timestamp):
-    readable_time = datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
-    return readable_time
-
 
 
 def delete_record(id, answer=False):
--- server.py
+++ server.py
@@ -19,18 +19,18 @@ def list_questions():
     try:
         order_by = request.args.get('order_by')
         order_direction = False if request.args.get('order_direction') == 'asc' else True
-        sorted_questions = data_handler.sorting_data(questions, order_by, order_direction)
+        sorted_questions = util.sorting_data(questions, order_by, order_direction)
         order_direction = 'asc' if order_direction == False else 'desc'
     except:
         order_by = 'submission_time'
         order_direction = 'desc'
-        sorted_questions = data_handler.sorting_data(questions, 'submission_time', True)
+        sorted_questions = util.sorting_data(questions, 'submission_time', True)
     return render_template('list.html',
                            fieldnames=fieldnames,
                            sorted_questions=sorted_questions,
                            order_by=order_by,
                            order_direction=order_direction,
-                           convert_to_readable_date=data_handler.convert_to_readable_date)
+                           convert_to_readable_date=util.convert_to_readable_date)
 
 
 
@@ -40,12 +40,12 @@ def add_question():
         req = request.form.to_dict()
         questions = data_handler.get_questions()
         handle_upload(req)
-        question = data_handler.generate_question_dict(req)
+        question = util.generate_question_dict(req)
         questions.append(question)
         data_handler.add_entry(question)
         return redirect(url_for("list_questions"))
 
-    return render_template("add-question.html", qid="")
+    return render_template("edit-question.html", qid="")
 
 
 @app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
@@ -53,7 +53,7 @@ def add_answer(question_id):
     if request.method == 'POST':
         reqv = request.form.to_dict()
         handle_upload(reqv)
-        answer = data_handler.generate_answer_dict(reqv)
+        answer = util.generate_answer_dict(reqv)
         answers = data_handler.get_answers()
 
         answers.append(answer)
@@ -70,7 +70,7 @@ def question_display(question_id):
     question = data_handler.get_question(question_id, question_database)
 
     related_answers = data_handler.get_question_related_answers(question_id, answer_database)
-    return render_template('display_question.html', question=question, answers=related_answers, convert_to_readable_date=data_handler.convert_to_readable_date)
+    return render_template('display_question.html', question=question, answers=related_answers, convert_to_readable_date=util.convert_to_readable_date)
 
 @app.route("/question/<question_id>/vote-up")
 def vote_up_question(question_id):
@@ -115,7 +115,7 @@ def edit_question(question_id):
         edited_question_data['submission_time'] = str(int(time.time()))
         question = data_handler.update_questions(question_id, edited_question_data)
         related_answers = data_handler.get_question_related_answers(question_id, data_handler.get_answers())
-        return render_template('display_question.html', question=question, answers=related_answers, convert_to_readable_date=data_handler.convert_to_readable_date)
+        return render_template('display_question.html', question=question, answers=related_answers, convert_to_readable_date=util.convert_to_readable_date)
 
     all_questions = data_handler.get_questions()
     question = data_handler.get_question(question_id, all_questions)
